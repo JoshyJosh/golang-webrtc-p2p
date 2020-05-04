d969e4f1a3a800da3e24ab083b481f9aab6424fa
joshyjosh <herukula@gmail.com>
2020-05-05 00:31:39 +0200

    add initial test for basic prototyping

--- selenium/webrtc_tests.js
+++ selenium/webrtc_tests.js
@@ -1,4 +1,5 @@
 const { buildDriver } = require('./webdriver')
+const test = require('tape')
 
 let driverCaller = buildDriver('firefox', undefined, false)
 let driverReceiver = buildDriver('firefox', undefined, false)
@@ -6,7 +7,7 @@ let driverReceiver = buildDriver('firefox', undefined, false)
 console.log(driverCaller)
 console.log(driverReceiver)
 
-async function getWebRTCPage() {
+async function getWebRTCPage(t) {
     try {
         await driverCaller.get('http://127.0.0.1:3000/')
         await driverReceiver.get('http://127.0.0.1:3000/')
@@ -22,15 +23,28 @@ async function getWebRTCPage() {
         driverCaller.sleep(2000)
 
         await driverCaller.executeScript("return pc")
-                          .then(function(pcVar){
-                              console.log(pcVar)
+                          .then(function(pc){
+                              console.log(pc)
+                              t.ok(pc.signalingState === "stable", "assert that Caller has established webRTC connection")
                           })
+        
+        await driverReceiver.executeScript("return pc")
+                            .then(function(pc){
+                                console.log(pc)
+                                t.ok(pc.signalingState === "stable", "assert that Receiver has established webRTC connection")
+                            })
     } catch(err) {
         console.log(err)
     } finally {
         driverCaller.quit()
         driverReceiver.quit()
     }
+
+    t.end()
 }
 
-getWebRTCPage()
+
+
+test("Firefox-FullConnection", t => {
+    getWebRTCPage(t)
+})
