e4fdce90b30046bd06256ab0cfde8af1a88cb2dc
joshyjosh <herukula@gmail.com>
2020-05-08 00:42:36 +0200

    redefineCheckStablePeerConnection, still need to create proper wait function for established connection

--- selenium/webrtc_tests.js
+++ selenium/webrtc_tests.js
@@ -1,84 +1,123 @@
 const { buildDriver } = require('./webdriver')
 const test = require('tape')
+const {until} = require('selenium-webdriver/lib/until')
+
+class ReconnectPeerError extends Error {}
 
 async function getWebRTCConnection(t, caller, receiver) {
     try {
         await caller.get('http://127.0.0.1:3000/')
         await receiver.get('http://127.0.0.1:3000/')
 
-        title = await caller.getTitle()
-        console.log(title)
-        
-        source = await caller.getPageSource()
-        console.log(source)
-
         await CheckStablePeerConnections(t, caller, receiver)
     } catch (err) {
-        console.log(err)
+        throw err
     }   
 }
 
 async function ReconnectPeer(t, peer) {
-    peer.close()
-    peer.get('http://127.0.0.1:3000/')
+    try {
+        await peer.navigate().refresh()
+        
+        await peer.executeScript("return pc")
+                    .then(function(pc){
+                        t.ok(pc.signalingState === "stable", "assert that peer connection has been reestablished")
+                    })
+    } catch (err) {
+        throw new ReconnectPeerError(err)
+    }
 }
 
 async function CheckStablePeerConnections(t, caller, receiver, additionalComment) {
-    callerComment = "assert that Caller has established webRTC connection"
-    receiverComment = "assert that Receiver has established webRTC connection"
-
-    // add additional commment to distinguish between test scenarios
     if (additionalComment) {
-        callerComment += " " + additionalComment
-        receiverComment += " " + additionalComment
+        additionalComment = " " + additionalComment
+    } else {
+        additionalComment = ""
     }
 
-    await caller.executeScript("return pc")
-                      .then(function(pc){
-                          console.log(pc)
-                          t.ok(pc.signalingState === "stable", callerComment)
-                      })
+    try {
+        // wait for peer connection to get established
+        await caller.executeScript(`
+            let checkLocalDescription = setInterval(() => {
+                if (window.pc.currentRemoteDescription) {
+                    // do something
+                    if (window.pc.currentRemoteDescription.type === "offer") {
+                        clearInterval(checkLocalDescription);
+                    }
+                }
+            },500);
+            
+            setTimeout(() => { clearInterval(checkLocalDescription)}, 5000)
+        `)
+        await caller.executeScript("return pc") 
+                    .then(function(pc){
+                        t.ok(pc.signalingState === "stable", "assert that Caller has established webRTC connection" + additionalComment)
+                        return pc
+                    })
+                    .then(function(pc){
+                        t.ok(pc.currentLocalDescription !== null, "assert that Caller PeerConnection has non-null currentLocalDescription" + additionalComment)
+                        t.equal(pc.currentLocalDescription.type, "offer", "assert that Caller PeerConnection has currentLocalDescription of type offer" + additionalComment)
+                    })
+                    .then(function(pc){
+                        t.ok(pc.currentRemoteDescription !== null, "assert that Caller PeerConnection has non-null currentRemoteDescription" + additionalComment)
+                        t.equal(pc.currentRemoteDescription.type, "answer", "assert that Caller PeerConnection has currentRemoteDescription of type answer" + additionalComment)
+                    })
+                    .catch(err => {
+                        throw err
+                    })
     
-    await receiver.executeScript("return pc")
-                        .then(function(pc){
-                            console.log(pc)
-                            t.ok(pc.signalingState === "stable", receiverComment)
-                        })
+        await receiver.executeScript("return pc")
+                      .then(function(pc){
+                          t.ok(pc.signalingState === "stable", "assert that Receiver has established webRTC connection" + additionalComment)
+                      })
+                      .then(function(pc){
+                          t.ok(pc.currentLocalDescription !== null, "assert that Receiver PeerConnection has non-null currentLocalDescription" + additionalComment)
+                          t.equal(pc.currentLocalDescription.type, "answer", "assert that Receiver PeerConnection has currentLocalDescription of type answer" + additionalComment)
+                      })
+                      .then(function(pc){
+                          t.ok(pc.currentRemoteDescription !== null, "assert that Receiver PeerConnection has non-null currentRemoteDescription" + additionalComment)
+                          t.equal(pc.currentRemoteDescription.type, "offer", "assert that Receiver PeerConnection has currentRemoteDescription of type offer" + additionalComment)
+                      })
+                      .catch(err => {
+                          throw err
+                      })
+    } catch(err) {
+        throw err
+    }
 }
 
-test("Firefox-SuccessfulConnection", t => {
+test("Firefox-SuccessfulConnection", async function (t){
     var driverCaller = buildDriver('firefox', undefined, false)
     var driverReceiver = buildDriver('firefox', undefined, false)
 
     try {
-        (async function(t, driverCaller, driverReceiver) {
-            await getWebRTCConnection(t, driverCaller, driverReceiver)
-            driverCaller.quit()
-            driverReceiver.quit()
-            t.end()
-        })(t, driverCaller, driverReceiver)
+        await getWebRTCConnection(t, driverCaller, driverReceiver)
     } catch(err) {
-        console.log(err)
-        driverCaller.quit()
-        driverReceiver.quit()
+        console.error(err)
+    } finally {
+        await driverCaller.quit()
+        await driverReceiver.quit()
         t.end()
-    }   
+    }
 })
 
-// test("Firefox-CallerReconnect", t => {
-//     let driverCaller = buildDriver('firefox', undefined, false)
-//     let driverReceiver = buildDriver('firefox', undefined, false)
-
-//     try {
-//         getWebRTCConnection(t, driverCaller, driverReceiver)
-//         ReconnectPeer(t, driverCaller)
-//         CheckStablePeerConnections(t, driverCaller, driverReceiver, "after caller reconnect")
-//     } catch(err) {
-//         console.log(err)
-//     } finally {
-//         driverCaller.quit()
-//         driverReceiver.quit()
-//     }
-
-//     t.end()
-// })
+test("Firefox-CallerReconnect", async function (t){
+    var driverCaller = await buildDriver('firefox', undefined, false);
+    var driverReceiver = await buildDriver('firefox', undefined, false);
+    
+    try {
+        await getWebRTCConnection(t, driverCaller, driverReceiver)
+        await ReconnectPeer(t, driverCaller)
+        await CheckStablePeerConnections(t, driverCaller, driverReceiver, "after caller reconnect")
+    } catch(err) {
+        if (err instanceof ReconnectPeerError) {
+            console.log("Peer connection error")
+            await driverCaller.quit()
+        }
+        console.error(err)
+    } finally {
+        await driverCaller.quit()
+        await driverReceiver.quit()
+        t.end()
+    }
+})
